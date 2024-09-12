32665c0b5310689584c116670e857720d90690ba
batyr <shutenovbatyrkhan@gmail.com>
2024-09-12 15:08:53 +0500

    fixed issue with get ID by SQL ORM

--- app/models.py
+++ app/models.py
@@ -2,11 +2,11 @@ from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from .database import Base
 from datetime import datetime
 
-# Модель для таблицы tp_dict
 class TpDict(Base):
     __tablename__ = 'tp_dict'
     
-    tp_code = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
+    tp_code = Column(Integer, unique=True, index=True)
     tp_name = Column(String, nullable=False)
     tp_status = Column(String, nullable=False)
     is_commercial = Column(Boolean, default=False)
